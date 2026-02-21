b332edaded8bb79e2f0fef43d927dee648fee1bd
MOHAMMEDARMANALI <armanali807393@gmail.com>
2026-02-21 14:18:00 +0530

    Second commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const PORT = process.env.PORT;
+const AI_URL = process.env.AI_SERVICE_URL;
 const express = require("express");
 const cors = require("cors");
 
@@ -8,5 +11,21 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend running");
 });
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Hello from backend" });
+});
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
+
+const axios = require("axios");
+
+app.get("/api/ai-test", async (req, res) => {
+  try {
+    const response = await axios.get("http://localhost:8000");
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "AI service not reachable" });
+  }
+});
--- frontend/Ai-placement-Frontend/src/App.jsx
+++ frontend/Ai-placement-Frontend/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/test")
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.error(err));
+  }, []);
+  /* useEffect(() => {
+    fetch("http://localhost:5000/api/ai-test")
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.error(err));
+  }, []); */
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <h1>AI Placement Mentor</h1>
+  );
 }
 
-export default App
+export default App;
